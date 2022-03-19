a60332466f9913669fe1d91a57598bf66812b354
Renuja <devmindadeco@gmail.com>
2022-03-19 22:07:09 +0530

    initial commit7

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,11 @@ export default function Home() {
       <Head>
       <meta charset="UTF-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lalezar&display=swap" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css2?family=Lalezar&display=swap" rel="stylesheet"></link>
         
       <title>CRE8</title>
 
